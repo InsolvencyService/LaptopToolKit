5f5ff35f003a1e356c6dcb373b3e90d105a64867
GOV.UK Patterns & Tools CI User <patterns-and-tools-github-user@digital.cabinet-office.gov.uk>
2017-07-21 07:22:59 +0100

    ADS-20 Inital Install script & Commit

---
+++ install-ansible.sh
@@ -0,0 +1,22 @@
+#
+# script: install-ansible
+# Description: Setup ansible from PPA on Ubuntu Machine
+# Author: Tom Withers
+# Email: hello@tomwithers.me
+# Changelog: Initial version
+#
+
+echo
+echo "Updating apt lists."
+sudo apt-get update
+echo "Installing software-properties-common."
+sudo apt-get install -y software-properties-common
+
+echo "Installing Ansible dependencies."
+sudo apt-get install -y python-yaml python-jinja2 python-paramiko python-crypto
+
+echo "Installing ansible from ansible ppa repository."
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+echo "Ansible has been installed"
